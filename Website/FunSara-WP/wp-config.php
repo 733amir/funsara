69afb835eb9770862dab56b47b9af7251ef543d3
Iman Irandoost <imn.irdst@gmail.com>
2016-06-14 17:27:17 +0430

    Change wp-config due to moving to another PC
    5 post Images added to repository, Moved to my laptop.

--- Website/FunSara-WP/wp-config.php
+++ Website/FunSara-WP/wp-config.php
@@ -26,13 +26,13 @@ define('DB_NAME', 'wordpress');
 define('DB_USER', 'wordpressuser');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'imir122213219');
+define('DB_PASSWORD', 'Im!r122213219');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
+define('DB_CHARSET', 'utf8mb4');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
@@ -46,14 +46,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',         'dUKqn}kVx@^?BO[nSTKO@_YWyEzZ3^_0|c<(rq<#g&s$NW}aV23u&vWWu&8<dtE,');
+define('SECURE_AUTH_KEY',  'bYWG8R.>~Ho|QTe(_eo}X(NV{[g&qivP#nNzFhht/yuI_n^w8+_7L)4=sb.~s-.q');
+define('LOGGED_IN_KEY',    '=Li^Y)V)eE< Uufx3@)&RX*HPf`27hB[(5ur/SU#/:gVv&iTf|Yoj$yDi<]v`~Cf');
+define('NONCE_KEY',        '.xr*5A}jt@^G s?}`Mi2UM7RrF:>5x.xc}vw B[_qC]E.{R;>$Ew|pz,6!^cdp|}');
+define('AUTH_SALT',        'y>{|E!6%GgynW8T_]Lf.&D@T;-eP]Ioovjuu^{ }yEm#b})eX5tg%-nbBtuL&5Hr');
+define('SECURE_AUTH_SALT', 'g|(>lO2N-PBAOQvQO2v%<Aff7K[$_i}>jeS=jwOiY*mR`@5Kc4{.UiBJ8*Na #K ');
+define('LOGGED_IN_SALT',   '~H?V:&[HflbQdbllESmu!X`C<Qgp#ejD8Ng<7V)Hu#0RjA+?3eo8lRI4z&(7PD&H');
+define('NONCE_SALT',       ';^UAXHPV!$g&+9bJ,+zNRlXP_~W*2h DumyI]3p3&YiN8<MB]*W:B2uupJkLn+uZ');
 
 /**#@-*/
 
