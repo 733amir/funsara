5a27aa3a24092807e9a01eb70f722572f7f38b0f
Amir Khazaie <733amir@gmail.com>
2016-06-21 11:39:11 +0430

    Add __pycache__ to .gitignore. "/api/version" response is completed but not tested yet.

--- Server/server.py
+++ Server/server.py
@@ -1,26 +1,26 @@
 import cherrypy
+from database import Database
 
 # Lowest version of android application that user should have.
 APP_VERSION = 1
 
 
 class Server:
+    def __init__(self):
+        self.db = Database()
+
     @cherrypy.expose
     def api(self, path, number=None):
 
         # Called with `/api/version`
         if path == 'version':
-            # TODO:
-            # Fetch last batch number, Version of application.
-            # Build JSON object like:
-            # {
-            #   "lastBatchNumber": NUMBER-OF-LAST-BATCH,
-            #   "appVersion": APPLICATION-VERSION
-            # }
-            return '/api/version'
+            # Run a query to get most recent batch number
+            last_batch_number = self.db.run(('use funsara', 'SELECT MAX(number) FROM batches',))[1][0][0]
+
+            return '''{}"lastBatchNumber": {}, "appVersion": {}{}'''.format('{', last_batch_number, APP_VERSION, '}')
 
         # Called with `/api/batch?number=%d`
-        elif path == 'batch':  # TODO Check for `number` variable.
+        elif path == 'batch' and number is not None:
             # TODO:
             # Fetch last batch videos.
             # Build JSON array contains JSON object like:
@@ -34,6 +34,7 @@ class Server:
             #     "videoUrl": URL-OF-VIDEO
             #   }
             # for each video in the batch.
+
             return '/api/batch/?number={}'.format(number)
 
         # URLs that haven't been handle or requests with bad parameters.
